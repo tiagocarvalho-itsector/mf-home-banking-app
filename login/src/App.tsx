549bc6d614c0d31927ef2852073b9eeb349400d9
tiagocarvalho-itsector <tiago.carvalho@itsector.pt>
2025-03-26 12:47:18 +0000

    add logo to login page

--- login/src/App.tsx
+++ login/src/App.tsx
@@ -6,6 +6,11 @@ import { Route, Routes } from "react-router-dom";
 const App: React.FC = () => {
   return (
     <div className="login-page-background">
+      <img
+        src="https://i.ibb.co/rGVhGd1t/image-removebg-preview.png"
+        alt="image-removebg-preview"
+        className="login-bank-logo"
+      />
       <Routes>
         <Route path="/" element={<LoginForm />} />
       </Routes>
